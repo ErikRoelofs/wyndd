582be79dacba08b30a3e2401f77542792717bc07
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-06-13 18:36:48 +0200

    Power determines number of issues (0,1 or2)

--- faction.lua
+++ faction.lua
@@ -9,6 +9,16 @@ function newFaction(id, name, standing, power, potentials)
         self.issueFactory:addPotential(potential)
       end,
       revealNewIssues = function(self)
+        local chance1 = self.power * 5 + 50
+        if math.random(1,100) <= chance1 then
+          self:reveal()
+        end
+        local chance2 = self.power * 5
+        if math.random(1,100) <= chance2 then
+          self:reveal()
+        end
+      end,
+      reveal = function(self)
         local maybeIssue = self.issueFactory:getValidIssue()
         if maybeIssue then
           table.insert(issues, maybeIssue)

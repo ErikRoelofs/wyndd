1b72e961ab0f6da6ccb5b9f89cdee6b7d7fa1f14
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-06-22 17:40:43 +0200

    Made some ugly markers for faction

--- layout/faction.lua
+++ layout/faction.lua
@@ -4,8 +4,8 @@ return function(lc)
     
     local container = lc:build("linear", {direction = "v", width = "wrap", height = "wrap"})     
     container:addChild( lc:build( "text", {data = function() return options.name end, width="wrap", height="wrap", padding = lc.padding(5) }) )
-    container:addChild( lc:build( "numberasimage", {width = "wrap", height = "wrap", value = function() return options.power end, image = "assets/faith.png", emptyImage = "assets/food.png", maxValue = 10, backgroundColor = {255,0,0,255}}))
-    container:addChild( lc:build( "numberasimage", {width = "wrap", height = "wrap", value = function() return options.standing end, image = "assets/wealth.png", emptyImage = "assets/might.png", maxValue = 10, backgroundColor = {255,0,0,255}}))
+    container:addChild( lc:build( "numberasimage", {width = "wrap", height = "wrap", value = function() return options.power end, image = "assets/power.png", emptyImage = "assets/emptypower.png", maxValue = 10}))
+    container:addChild( lc:build( "numberasimage", {width = "wrap", height = "wrap", value = function() return options.standing end, image = "assets/heart.png", emptyImage = "assets/emptyheart.png", maxValue = 10}))
    
       return container
     end,
